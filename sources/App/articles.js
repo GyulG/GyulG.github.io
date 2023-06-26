1937735413b15333b802ccdea74bbb3499a022d7
GyulG <137787904+GyulG@users.noreply.github.com>
2023-06-26 23:58:51 +1200

    Add files via upload

---
+++ sources/App/articles.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    title: "Artwork 0",
+    image: "/assets/example-0.webp"
+  },
+  {
+    title: "Artwork 1",
+    image: "/assets/example-1.webp"
+  },
+  {
+    title: "Artwork 2",
+    image: "/assets/example-2.webp"
+  },
+  {
+    title: "Artwork 3",
+    image: "/assets/example-3.webp"
+  },
+  {
+    title: "Artwork 4",
+    image: "/assets/example-4.webp"
+  },
+  {
+    title: "Artwork 5",
+    image: "/assets/example-5.webp"
+  },
+  {
+    title: "Artwork 6",
+    image: "/assets/example-6.webp"
+  },
+  {
+    title: "Artwork 7",
+    image: "/assets/example-7.webp"
+  },
+  {
+    title: "Artwork 8",
+    image: "/assets/example-8.webp"
+  }
+];
